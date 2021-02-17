2c3c8b8a2ae556deb5ebfe2ecd0b0f5c7cbae827
Ersultan003 <73297409+Ersultan003@users.noreply.github.com>
2021-02-17 14:19:11 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const box = document.querySelector('.box');
+
+
+// const width = box.clientWidth;
+// const height = box.clientHeight;
+const width = box.offsetWidth;
+const height = box.offsetHeight;
+console.log(width, height);
